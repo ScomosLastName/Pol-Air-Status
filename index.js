4c1ff5a1cde71f2e79dd0aeeba11b181a63d6599
Sam <105767464+ScomosLastName@users.noreply.github.com>
2025-04-12 03:22:48 +1000

    Flight Logs
    Added the option to log flights in a separate / command defined channel. also added commented out code to remove old commands during development.

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const {Client, Events, SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle} = require("discord.js");
+const {
+    Client, 
+    Events, 
+    SlashCommandBuilder, 
+    EmbedBuilder, ButtonBuilder, 
+    ActionRowBuilder, ButtonStyle, 
+    REST, Routes //Final 2 are used when clearing commands
+} = require("discord.js");
 
+//CLEARS OLD COMMANDS
+
+/*
+const commands = []; // an empty array will clear all global commands
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Removing all global commands...');
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully removed all global commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+*/
 
 const client = new Client({intents: []});
 
@@ -21,6 +50,7 @@ var airWingAvaliable = true;
 var flightTimer;
 var refuelTimer;
 var pilot;
+var logChannelId;
 
 async function updateEmbed() {
     if (airWingDeployed && airWingAvaliable) {
@@ -47,18 +77,18 @@ async function updateEmbed() {
     }
 }
 // TODO correct the timing when out of testing
-function setFlightTimer(endTime, interaction, row) {
+function setFlightTimer(time, interaction, row) {
     clearTimeout(refuelTimer);
     console.log('timer initiated');
-    landTime = endTime;
-    flightTimer = setTimeout(outOfFuel, endTime - (Math.floor(Date.now)/1000) * 1000, interaction, row);
+    landTime = time/1000 + Math.floor(Date.now()/1000);
+    flightTimer = setTimeout(outOfFuel, time, interaction, row);
 }
 
-function setRefuelTimer(endTime, interaction, row) {
+function setRefuelTimer(time, interaction, row) {
     clearTimeout(flightTimer);
     console.log('timer initiated');
-    refuel = endTime;
-    refuelTimer = setTimeout(refueled, endTime - (Math.floor(Date.now)/1000) * 1000, interaction, row)
+    refuel = time/1000 + Math.floor(Date.now()/1000);
+    refuelTimer = setTimeout(refueled, time, interaction, row)
 }
 
 async function refueled(interaction, row) {
@@ -91,7 +121,12 @@ async function refueled(interaction, row) {
             }
         }
     
-        const reply = await interaction.channel.send({ embeds: [embed], fetchReply: true, components: [row] });
+        const reply = await interaction.channel.send({ 
+            embeds: [embed], 
+            fetchReply: true, 
+            components: [row] 
+        });
+
         embedMessageId = reply.id;
     }
     airWingAvaliable = true;
@@ -128,10 +163,36 @@ async function outOfFuel (interaction, row) {
         }
     }
 
-    const reply = await interaction.channel.send({ embeds: [embed], fetchReply: true });
+    const reply = await interaction.channel.send({ 
+        embeds: [embed], 
+        fetchReply: true 
+    });
     embedMessageId = reply.id;
 
     setRefuelTimer(refuelTime, interaction, row)
+}
+
+function createLogEmbed(pilot, status, i) {
+    const logEmbed = new EmbedBuilder()
+    if (status) {
+        logEmbed
+            .setColor(0x5865F2)
+            .setTitle('Flight log')
+            .setDescription('Pol Air has been deployed!')
+            .setThumbnail('https://i.ibb.co/G3rbYFxN/image.png')
+            .addFields({ name: 'Deployed By:', value: `<@${i.user.id}>`, inline: true })
+            .setTimestamp()
+    } else {
+        logEmbed
+            .setColor(0x5865F2)
+            .setTitle('Flight log')
+            .setDescription('Pol Air has been undeployed!')
+            .setThumbnail('https://i.ibb.co/G3rbYFxN/image.png')
+            .addFields({ name: 'Undeployed By:', value: `<@${i.user.id}>`, inline: true })
+            .setTimestamp()
+    }
+    
+    return logEmbed;
 }
 
 client.once(Events.ClientReady, c => {
@@ -147,9 +208,13 @@ client.once(Events.ClientReady, c => {
         .setName("toggle_pol_air")
         .setDescription("Deploys / Undeploys Pol Air")
 
+        const set_log_channel = new SlashCommandBuilder()
+            .setName('set_log_channel')
+            .setDescription('Sets the channel for deployment logs')
+
     client.application.commands.create(init_embed);
     client.application.commands.create(toggle_pol_air);
-    
+    client.application.commands.create(set_log_channel);
     updateEmbed();
 });
 
@@ -164,17 +229,30 @@ client.on(Events.InteractionCreate, async (interaction) => {
     .addComponents(deploy)
 
     if (interaction.commandName === "init_embed") {
-        const reply = await interaction.reply({ embeds: [embed], fetchReply: true, components: [row] });
+        const reply = await interaction.reply({
+            embeds: [embed],
+            fetchReply: true, 
+            components: [row] 
+        });
+
         embedMessageId = reply.id;
 
         console.log(embedMessageId)
     } else if (interaction.commandName === "toggle_pol_air" || interaction.isButton(deploy)) {
         airWingDeployed = !airWingDeployed;
+
+        if (logChannelId) {
+            const logChannel = await interaction.client.channels.fetch(logChannelId);
+            await logChannel.send({
+                embeds: [createLogEmbed(interaction.user.id, airWingDeployed, interaction)]
+            });
+        }
+        
         if (airWingDeployed) {
-            setFlightTimer(Math.floor(Date.now()/1000) + 5400, interaction, row);
+            setFlightTimer(5400000, interaction, row);
             pilot = interaction.user.id;
         } else {
-            setRefuelTimer(Math.floor(Date.now()/1000) + 1800, interaction, row);
+            setRefuelTimer(1800000, interaction, row);
             pilot = null;
         }
 
@@ -204,9 +282,18 @@ client.on(Events.InteractionCreate, async (interaction) => {
             }
         }
 
-        const reply = await interaction.reply({ embeds: [embed], fetchReply: true, components: [row]});
+        const reply = await interaction.reply({ 
+            embeds: [embed], 
+            fetchReply: true, 
+            components: [row]
+        });
         embedMessageId = reply.id;
-    }
-})
+    } else if (interaction.commandName === "set_log_channel") {
+        logChannelId = interaction.channelId;
+        interaction.reply({
+            content: `Set to <#${logChannelId}>`,
+            ephemeral: true
+        });    }
+});
 
 client.login(process.env.DISCORD_TOKEN);    
